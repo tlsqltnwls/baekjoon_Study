aedb190706a2ca129eb5cdd17dc192cd903bba42
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-05 04:58:55 +0900

    3일차

---
+++ Class1/1152/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String A = Console.ReadLine();
+            String[] B = A.Split(' ');
+
+            // int len = B.Length;
+            int count = 0;
+
+            if (B[0].Length == 0) count++;
+            if (B[B.Length - 1].Length == 0) count++;
+
+            Console.WriteLine(B.Length - count);
+        }
+    }
+}
