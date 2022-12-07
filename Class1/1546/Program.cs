0e614c7d5b253fd021e668a1df0cd8a01b7b3e36
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-08 01:49:31 +0900

    6일차

---
+++ Class1/1546/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _1546
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            String[] arr = Console.ReadLine().Split(' ');
+            float[] score = new float[N];
+
+            float max = 0;
+            float add = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                score[i] = int.Parse(arr[i]);
+
+                if (max < score[i]) max = score[i];
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                score[i] = (score[i] / max) * 100;
+                add += score[i];
+            }
+
+            Console.WriteLine(add / N);
+        }
+    }
+}
