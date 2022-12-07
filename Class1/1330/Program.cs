bb9d709708f96cde9f547bb7345058a8e4fce9cd
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-07 22:13:38 +0900

    5일차

---
+++ Class1/1330/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1330
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] str_tmp = Console.ReadLine().Split(' ');
+
+            if (Int32.Parse(str_tmp[0]) > Int32.Parse(str_tmp[1])) 
+                Console.WriteLine(">");
+            if (Int32.Parse(str_tmp[0]) < Int32.Parse(str_tmp[1])) 
+                Console.WriteLine("<");
+            if (Int32.Parse(str_tmp[0]) == Int32.Parse(str_tmp[1])) 
+                Console.WriteLine("==");
+            // else if 로 조건 나누는 것보다 그냥 if 여러개 쓰는게 더 빠름
+        }
+    }
+}
