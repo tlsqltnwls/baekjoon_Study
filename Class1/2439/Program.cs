338732a8146eb550e66b823d8e33dab6169c8c64
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-09 23:42:04 +0900

    7일차

---
+++ Class1/2439/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2439
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = N - i - 1; j > 0; j--)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int j = -1; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
