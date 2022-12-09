fb45dbf948dddb598567701cffc979804006cbb8
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-10 00:02:24 +0900

    8일차

---
+++ Class1/2475/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2475
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] unique_num = new double[5];
+            String[] temp = Console.ReadLine().Split(' ');
+            double sum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                unique_num[i] = double.Parse(temp[i]);
+                sum += Math.Pow(unique_num[i], 2);
+            }
+
+            Console.WriteLine(sum % 10);
+        }
+    }
+}
