e2359271a9a17d1ac6c62ffbd6e83b7ee76f34a9
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-11 19:21:33 +0900

    9일차

---
+++ Class1/2562/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2562
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[9];
+            int max = 0;
+            int index = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+
+                if (max < num[i])
+                {
+                    max = num[i];
+                    index = i + 1;
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine(index);
+        }
+    }
+}
---
+++ Class1/2577/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2577
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine()) * int.Parse(Console.ReadLine()) * int.Parse(Console.ReadLine());
+            int[] check = new int[10];
+
+            while (num > 0)
+            {
+                check[num % 10]++;
+                num /= 10;
+            }
+
+/*            string str_num = num.ToString();
+
+            for (int i = 0; i < str_num.Length; i++)
+            {
+                check[str_num[i] - 48]++;
+            }*/
+
+            foreach (var item in check)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
