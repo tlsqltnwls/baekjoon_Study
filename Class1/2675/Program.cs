db275191224eff3da838bebbdcd90396464e6d2c
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-12 22:01:25 +0900

    10일차

---
+++ Class1/2675/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2675
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                String[] S_arr = Console.ReadLine().Split(' ');
+                string S = S_arr[1];
+                int R = int.Parse(S_arr[0]);
+                int count = 0;
+
+                for (int j = 0; j < S.Length; j++)
+                {
+                    count = 0;
+                    while (count < R)
+                    {
+                        Console.Write(S[j]);
+                        count++;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Class1/2739/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2739
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(N + " * " + (i + 1) + " = " + (N * (i + 1)));
+            }
+        }
+    }
+}
