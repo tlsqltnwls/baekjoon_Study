53f22cd58709759b230bdb9ab464d79d7827caec
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-20 01:54:12 +0900

    14일차
    9498
    10171
    10172
    10809

---
+++ Class1/10171/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _10171
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"\    /\");
+            Console.WriteLine(@" )  ( ')");
+            Console.WriteLine(@"(  /  )");
+            Console.WriteLine(@" \(__)|");
+        }
+    }
+}
---
+++ Class1/10809/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10809
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int[] eng = new int[26];
+
+            for (int i = 0; i < 26; i++) eng[i] = -1;
+            for (int i = S.Length - 1; i > -1; i--) eng[S[i] - 97] = i;
+
+            foreach (var item in eng) Console.Write(item + " ");
+        }
+    }
+}
