b11c7aff582c996aacdba42ac6edf129a516f056
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2022-12-19 16:44:36 +0900

    13일차
    3052
    8958

---
+++ Class1/3052/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[42];
+            int count = 0;
+
+            for (int i = 0; i < 10; i++)
+                num[(int.Parse(Console.ReadLine()) % 42)]++;
+            for (int i = 0; i < 42; i++)
+                if (num[i] > 0) count++;
+
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ Class1/8958/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _8958
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int test_size = int.Parse(Console.ReadLine());
+            string testOX;
+            int score = 0;
+            int sum = 0;
+
+            for (int i = 0; i < test_size; i++)
+            {
+                testOX = Console.ReadLine();
+
+                for (int j = 0; j < testOX.Length; j++)
+                {
+                    if (testOX[j] == 'O') sum += ++score;
+                    else score = 0;
+                }
+
+                Console.WriteLine(sum);
+                score = 0;
+                sum = 0;
+            }
+        }
+    }
+}
