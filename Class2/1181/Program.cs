353c772edcbb4e7c79c18f997e22bdb79f6d7ab7
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2023-03-30 13:46:32 +0900

    1181 1259
    1181은 어제 풀었는데 오늘 올림..

--- Class2/1181/Program.cs
+++ Class2/1181/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _1181
 {
@@ -8,6 +9,24 @@ namespace _1181
         {
             int N = int.Parse(Console.ReadLine());
             string[] arr = new string[N];
+
+            for (int i = 0; i < N; i++)
+                arr[i] = Console.ReadLine();
+
+            string[] distArr = arr.Distinct().ToArray();
+            // .ToArray() : List를 배열로 변환하는 방법
+            // IEnumerable 인터페이스에서 배열로 변환해주기 위해 .ToArray() 사용
+
+            Array.Sort(distArr);
+            // 사전 순 정렬
+            distArr = distArr.OrderBy(a => a.Length).ToArray();
+            // 길이가 짧은 순 정렬
+                       
+            foreach (var item in distArr)
+                Console.WriteLine(item);
         }
     }
 }
+
+
+// 참고 사이트 : https://bit.ly/3ZjPGVn
---
+++ Class2/1259/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _1259
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numList = new List<string>();
+            string n;
+
+            while (true)
+            {
+                n = Console.ReadLine();
+                if (n == "0") break;
+                numList.Add(n);
+            }
+
+            string[] num = numList.ToArray();
+            string[] compareNum = new string[numList.Count];
+
+            for (int i = 0; i < numList.Count; i++)
+            {
+                compareNum[i] = Reverse(num[i]);
+
+                if (string.Equals(compareNum[i], num[i]))
+                    Console.WriteLine("yes");
+                else
+                    Console.WriteLine("no");
+            }
+        }
+
+        public static string Reverse(string str)
+        {
+            char[] chars = str.ToCharArray();
+            Array.Reverse(chars);
+
+            return new string(chars);
+            // chars.ToString() : 사용 가능한 문자열이 아닌 개체 유형을 나타내는 문자열을 만듦
+            // new string(chars) : char 배열에 있는 모든 문자로 문자열을 만듦
+        }
+    }
+}
