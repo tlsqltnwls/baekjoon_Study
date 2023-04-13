600ce890b85a143bed65b70c8160031a2f48d377
tlsqltnwls <95041674+tlsqltnwls@users.noreply.github.com>
2023-04-13 21:24:39 +0900

    1920

---
+++ Class2/1874/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1874
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] sequence = new int[n];
+            int[] stack = new int[n];
+
+            //for (int i = 0; i < n; i++)
+            //    sequence[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("전: " + n);
+            test(n);
+            Console.WriteLine("후: " + n);
+        }
+
+        static void test (int n)
+        {
+            n = 5;
+            Console.WriteLine(" 테스트 중: " + n);
+        }
+    }
+}
---
+++ Class2/1920/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace _1920
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader strR = new StreamReader(Console.OpenStandardInput());
+            StreamWriter strW = new StreamWriter(Console.OpenStandardOutput());
+
+            int N = int.Parse(strR.ReadLine());
+            int[] N_num = Array.ConvertAll(strR.ReadLine().Split(' '), int.Parse);
+            // Array.ConvaerAll(여기 있는걸, 작성한 형으로 전부 바꿔줌)
+            Array.Sort(N_num);
+
+            int M = int.Parse(strR.ReadLine());
+            int[] M_num = Array.ConvertAll(strR.ReadLine().Split(' '), int.Parse);
+
+            for (int i = 0; i < M; i++)
+                strW.WriteLine(binary(M_num[i], N_num));
+
+            strR.Close();
+            strW.Close();
+        }
+
+        static int binary(int value, int[] N)
+            // 이진 탐색
+        {
+            int left = 0;
+            int right = N.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (value < N[mid])
+                    right = mid - 1;
+                else if (value > N[mid])
+                    left = mid + 1;
+                else return 1;
+            }
+
+            return 0;
+        }
+    }
+}
